c9afc672acf4127904ef2f726fd7033eacd2c0b1
Laura Bucchieri <laurabucchieri@ip-172-31-20-196.us-east-2.compute.internal>
2021-04-27 12:11:10 -0500

    login display stuff

--- messaging_app_client/src/App.js
+++ messaging_app_client/src/App.js
@@ -9,13 +9,15 @@ import NewMessage from './components/NewMessage';
 import ShowConversationList from './components/ShowConversationList';
 import ShowMessageList from './components/ShowMessageList';
 import GetMessage from './components/GetMessage';
+import showLogin from './components/showLogin';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route path='/' component={ShowMessageList} />
+          <Route path='/' component={showLogin} />
+          <Route path='/message-list' component={ShowMessageList} />
           <Route path='/new-message' component={NewMessage} />
           <Route path='/get-message/:id' component={GetMessage} />
         </div>
