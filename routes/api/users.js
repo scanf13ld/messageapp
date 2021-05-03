5b2a7042f958bad076adf5df2d66e4486f2957fa
shane <canfield1@kenyon.edu>
2021-05-03 00:25:26 -0500

    more

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,49 +2,52 @@
 //CSE 330 Creative Project
 //Shane Canfield and Laura Bucchieri
 
+//Acknowledgements to https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-1-c405048e3669
+
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
-// Load input validation
+
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
-// Load User model
+
 const User = require("../../models/Users");
 
 // @route POST api/users/register
 // @desc Register user
 // @access Public
 router.post("/register", (req, res) => {
-  // Form validation
-const { errors, isValid } = validateRegisterInput(req.body);
-// Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-User.findOne({ username: req.body.username }).then(user => { //look in database for username
-    if (user) {
-      return res.status(400).json({ username: "username already exists" });
-    } else {
-      const newUser = new User({
-        username: req.body.username,
-        name: req.body.name,
-        password: req.body.password
-      });
+  
+	const { errors, isValid } = validateRegisterInput(req.body);
 
-      bcrypt.genSalt(10, (err, salt) => { // Hash password before saving in database
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser
-            .save()
-            .then(user => res.json(user))
-            .catch(err => console.log(err));
-        });
-      });
-    }
-  });
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
+	
+	User.findOne({ username: req.body.username }).then(user => { //look in database for username
+		if (user) {
+		  return res.status(400).json({ username: "username already exists" });
+		} else {
+		  const newUser = new User({
+			username: req.body.username,
+			name: req.body.name,
+			password: req.body.password
+		  });
+
+		  bcrypt.genSalt(10, (err, salt) => { // Hash password before saving in database
+			bcrypt.hash(newUser.password, salt, (err, hash) => {
+			  if (err) throw err;
+			  newUser.password = hash;
+			  newUser
+				.save()
+				.then(user => res.json(user))
+				.catch(err => console.log(err));
+			});
+		  });
+		}
+	});
 });
 
 // @route POST api/users/login
