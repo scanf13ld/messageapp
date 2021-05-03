884c7505d28a7328203de28826356a21202d683f
shane <canfield1@kenyon.edu>
2021-05-03 09:19:54 -0500

    pre-demo

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,29 +19,23 @@ const User = require("../../models/Users");
 // @desc Register user
 // @access Public
 router.post("/register", (req, res) => {
-  
-	const { errors, isValid } = validateRegisterInput(req.body);
-
-	if (!isValid) {
-		return res.status(400).json(errors);
-	}
-	
+  	
 	User.findOne({ username: req.body.username }).then(user => { //look in database for username
 		if (user) {
 		  return res.status(400).json({ username: "username already exists" });
 		} else {
-		  const newUser = new User({
+		  const newUser = new User({ //begin generatinge newuser from schema
 			username: req.body.username,
 			name: req.body.name,
 			password: req.body.password
 		  });
 
-		  bcrypt.genSalt(10, (err, salt) => { // Hash password before saving in database
+		  bcrypt.genSalt(10, (err, salt) => { // hash password before saving in database
 			bcrypt.hash(newUser.password, salt, (err, hash) => {
 			  if (err) throw err;
 			  newUser.password = hash;
 			  newUser
-				.save()
+				.save() //save into database
 				.then(user => res.json(user))
 				.catch(err => console.log(err));
 			});
@@ -55,49 +49,43 @@ router.post("/register", (req, res) => {
 // @access Public
 router.post("/login", (req, res) => {
   
-	const { errors, isValid } = validateLoginInput(req.body); // Form validation
-
-	if (!isValid) {
-		return res.status(400).json(errors);
-	}
 	const username = req.body.username;
 	const password = req.body.password;
 
 	User.findOne({ username }).then(user => {
  
-    if (!user) {
-      return res.status(404).json({ usernamenotfound: "username not found" });
-    }
-	
-    bcrypt.compare(password, user.password).then(isMatch => { //if password matches
-      if (isMatch) { //finds user
-       
-        
-        const payload = {
-          id: user.id,
-          name: user.name,
-		  username: user.username
-        };
+		if (!user) {
+		  return res.status(404).json({ usernamenotfound: "username not found" });
+		}
+		
+		bcrypt.compare(password, user.password).then(isMatch => { //if password matches
+		  if (isMatch) { //finds user
+		   
+			
+			const payload = {
+			  id: user.id,
+			  name: user.name,
+			  username: user.username
+			};
 
-        jwt.sign( 
-          payload,
-          keys.secretOrKey,
-          {
-            expiresIn: 31556926 // 1 year in seconds
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token
-            });
-          }
-        );
-      } else {
-        return res
-          .status(400)
-          .json({ passwordincorrect: "Password incorrect" });
-      }
-    });
+			jwt.sign( 
+			  payload,
+			  keys.secretOrKey,
+			  {
+				expiresIn: 1440 // a day
+			  },
+			  (err, token) => {
+				res.json({
+				  success: true,
+				  token: "Bearer " + token
+				});
+			  }
+			);
+		  } else {
+			return res
+			  .status(400).json({ passwordincorrect: "Password incorrect" });
+		  }
+		});
   });
 });
 
